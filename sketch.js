9fe5d132a0bf674e9ee11263e459033e68261732
quangvkl10x <43781340+quangvkl10x@users.noreply.github.com>
2020-05-04 22:15:30 +0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,36 +1,105 @@
+let scl=10;
+let player=new ship();
+let enemy=[];
+enemy[0]=new ship();
+let total_enemy=1;
+let dead=0;
+let shownEnemy= new Array(110).fill(false);
+let ifend=0;
+let supply_hp=new bullet();
 function setup(){
-	frameRate(30);
-	createCanvas(700,700);
-	setInterval(add,s.total*200);
+	frameRate(45);
+	for (let bad of enemy){
+		bad.x=Math.floor(Math.random()*1000);
+		bad.y=Math.floor(Math.random()*100);
+		bad.y=bad.y - bad.y%scl+scl;
+		bad.x=bad.x - bad.x%scl+scl;
+		bad.shot.x=bad.x;
+		bad.shot.y=bad.y;
+		shownEnemy[bad.y]=true;
+
+	}
+	createCanvas(1200,710);
+	setInterval(add,player.total*200);
 }
-var s=new ship();
 function add(){
-		s.total++;
-		s.ammo[s.total-1]=new bullet();
-		s.ammo[s.total-1].x=s.x;
-		s.ammo[s.total-1].y=s.y-scl;
+		player.total++;
+		player.ammo[player.total-1]=new bullet();
+		player.ammo[player.total-1].x=player.x;
+		player.ammo[player.total-1].y=player.y-scl;
 		clearInterval(add);
-		console.log(s.total);	
 }
-var scl=10;
 function keyPressed(){
 	if (keyCode == UP_ARROW){
-		s.go(0,-1);
+		player.go(0,0);
 	}
 	else if(keyCode == DOWN_ARROW){
-		s.go(0,1);
+		player.go(0,0);
 	}
 	else if(keyCode == LEFT_ARROW){
-		s.go(-1,0);
+		player.go(-1,0);
 	}
 	else if (keyCode == RIGHT_ARROW){
-		s.go(1,0);
+		player.go(1,0);
 	}
 }
 function draw(){
 	background(50);
 	fill(255);
-	
-	s.update();
-	s.show();
+	player.update();
+	player.showPlayer();
+	dead=0;
+	ifend=0;
+	for (let bad of enemy){
+		fill(255);
+		if (bad.hp>0){
+		bad.showEnemy();
+		if (bad.shot.hurt(player)){
+			bad.shot.y+=500;
+		}
+		}
+		else {
+			fill(50);
+			stroke(50);
+			bad.showEnemy();
+			ifend++;
+			if (bad.shot.hurt(player)){
+				bad.shot.y+=500;
+			}
+			if (bad.shot.y>700)
+				ifend--;
+			dead++;
+		}
+	}
+	if (dead==total_enemy && ifend==0)
+	{
+		total_enemy++;
+		shownEnemy= new Array(500).fill(false);
+		enemy[total_enemy-1]=new ship();
+		dead=0;
+		for (let bad of enemy){
+		bad.hp=5;
+		bad.x=Math.floor(Math.random()*1000);
+		bad.y=Math.floor(Math.random()*100);
+		bad.y=bad.y - bad.y%scl+scl;
+		bad.x=bad.x - bad.x%scl+scl;
+		if (shownEnemy[bad.y]) 
+			{
+				bad.y=Math.floor(Math.random()*100);
+				bad.y=bad.y - bad.y%scl+scl;
+			}
+		else shownEnemy[bad.y]=true;
+		bad.shot.x=bad.x;
+		bad.shot.y=bad.y;
+		}
+	}
+	if (player.hp < 1)
+	{
+		alert("YOU LOSE!!");
+		location.reload();
+	}
+	for (let i=1;i<=player.hp;i++){
+		fill(255,0,0);
+		rect(1200-i*scl,700,scl,scl);
+	}
 }
